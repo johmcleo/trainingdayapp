079406f953757af9108e6f511d95feaed71eb04d
John <johmcleo@cisco.com>
2018-03-27 10:47:37 -0700

    database config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://mongo.ciscotrainingday.io/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
